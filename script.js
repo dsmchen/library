6215dbbd05605b57d0965f6e9c2a860eecb40bbf
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-08-27 14:24:43 +0800

    Add book functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const fantasyLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  let book = new Book(title, author, pages, read);
+  fantasyLibrary.push(book);
+}
